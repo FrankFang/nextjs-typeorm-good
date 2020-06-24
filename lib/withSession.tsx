dffc0eaa58daa692fb52fe61c75b90754180acb5
frankfang <frankfang1990@gmail.com>
2020-06-24 23:02:56 +0800

    使用 .env.local 设置环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,8 +3,7 @@ import {NextApiHandler} from 'next';
 
 export function withSession(handler: NextApiHandler) {
   return withIronSession(handler, {
-    // password: process.env.SECRET_COOKIE_PASSWORD,
-    password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da',
+    password: process.env.SECRET,
     cookieName: 'blog',
     cookieOptions: {secure: false}
   });
