83a1356c80e456c8365a77746a2045656745b241
frankfang <frankfang1990@gmail.com>
2020-07-01 23:15:18 +0800

    优化首页代码复用

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,34 +1,39 @@
-import {GetStaticProps, NextPage} from 'next';
-import {usePosts} from '../../hooks/usePosts';
-import {useCallback} from 'react';
-import {getPosts} from '../../lib/posts';
+import {GetServerSideProps, NextPage} from 'next';
+import {UAParser} from 'ua-parser-js';
+import {getDatabaseConnection} from 'lib/getDatabaseConnection';
+import {Post} from 'src/entity/Post';
 import Link from 'next/link';
 
 type Props = {
-  posts: Post[];
+  posts: Post[]
 }
 const PostsIndex: NextPage<Props> = (props) => {
   const {posts} = props;
   return (
     <div>
       <h1>文章列表</h1>
-      {posts.map(p => <div key={p.id}>
-        <Link href={`/posts/${p.id}`}>
-          <a>
-            {p.id}
-          </a>
-        </Link>
-      </div>)}
+      {posts.map(post =>
+        <div>
+          <Link key={post.id} href={`/posts/${post.id}`}>
+            <a>
+              {post.title}
+            </a>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
-
 export default PostsIndex;
 
-export const getStaticProps = async () => {
-  const posts = await getPosts();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const connection = await getDatabaseConnection();// 第一次链接能不能用 get
+  const posts = await connection.manager.find(Post);
+  const ua = context.req.headers['user-agent'];
+  const result = new UAParser(ua).getResult();
   return {
     props: {
+      browser: result.browser,
       posts: JSON.parse(JSON.stringify(posts))
     }
   };
