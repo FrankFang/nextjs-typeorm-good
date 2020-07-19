4739300f3f069132ee55a58685db7064c31d228e
frankfang <frankfang1990@gmail.com>
2020-07-20 04:00:27 +0800

    添加首页

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
-import PostsIndex, {getServerSideProps} from './posts';
+import {NextPage} from 'next';
+import Link from 'next/link';
 
-export default PostsIndex;
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="cover">
+        <img src="/logo.png" alt=""/>
+        <h1>方方的个人博客</h1>
+        <p>我是一个装逼的人</p>
+        <p><Link href="/posts"><a>文章列表</a></Link></p>
+      </div>
+      <style jsx>{`
+      .cover{
+        height: 100vh;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      `}</style>
+    </>
+  );
+};
 
-export {getServerSideProps};
+export default Home;
+
