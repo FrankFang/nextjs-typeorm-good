ba3e1158225125f114657e8ff12b2167682aa16c
frankfang <frankfang1990@gmail.com>
2020-07-20 22:51:11 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,8 @@ git pull &&
 yarn install --production=false &&
 yarn build &&
 docker build -t fang/node-web-app . &&
+docker kill app &&
+docker rm app &&
 docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
 echo 'OK!'
 
