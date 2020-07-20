2a8d54875c8af0d4a43192e1a6e340d096a4e380
frankfang <frankfang1990@gmail.com>
2020-07-20 22:42:00 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-echo Hello > /tmp/frank
+docker start 98a &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t fang/node-web-app . &&
+docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+echo 'OK!'
+
