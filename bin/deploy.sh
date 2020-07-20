ffeab151eb57542eea1db18fa5fa5f8a375fc03e
frankfang <frankfang1990@gmail.com>
2020-07-20 23:16:19 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,8 @@ docker start 98a &&
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
-yarn build &&
 git apply migrate.patch &&
+yarn build &&
 yarn m:run &&
 git reset --hard HEAD &&
 docker build -t fang/node-web-app . &&
