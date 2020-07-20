7575d2bb581b36e8681ee3e7c3be1fe46d5a4ec2
frankfang <frankfang1990@gmail.com>
2020-07-20 22:53:22 +0800

    update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <div className="cover">
         <img src="/logo.png" alt=""/>
         <h1>方方的个人博客</h1>
-        <p>我是一个装逼的人</p>
+        <p>我是一个爱学习的人</p>
         <p><Link href="/posts"><a>文章列表</a></Link></p>
       </div>
       <style jsx>{`
